4eda95b3a2445b199a3e9f54ba6e42f3fcaf162e
MarcisKraukis1 <kraukis@gmail.com>
2018-02-13 21:36:20 +0200

    created my hello world app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //izsaucam savu izveidoto funkciju
+            SayHello();
+            //Izsaucam otro funkciju
+            SayHelloToUser();
+
+            
+        }
+
+        static void SayHelloToUser()
+        {
+            //Palūdzam lietotājam ievadīt savu vārdu
+            //uzrakstot lūgumu ekrānā
+            Console.WriteLine("Please enter your name:");
+            //izveidojam mainīgo, kur glabāt lietotāja ievadīto vērtību
+            string usersName;
+            //paprasam konsolei ievadīt tekstu, saglabājam rezultātu mainīgo
+            usersName = Console.ReadLine();
+            Console.WriteLine("Labdien," + usersName + "!"); //"Labdien, Marcis"
+            Console.ReadLine();
+        }
+       
+
+        static void SayHello()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+  }
+}
